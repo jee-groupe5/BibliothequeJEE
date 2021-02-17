ad7cbd740b5d78c3faa3f99f0ca213c01d5a32f5
MaxenceGrenot <maxence.grenot@epsi.fr>
2021-02-17 14:30:35 +0100

    Initialisation spring boot

---
+++ mediatech/src/test/java/JeeGrp5/mediatech/MediatechApplicationTests.java
@@ -0,0 +1,13 @@
+package JeeGrp5.mediatech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediatechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
