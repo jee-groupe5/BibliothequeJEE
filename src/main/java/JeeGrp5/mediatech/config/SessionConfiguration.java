efc78fd6fdf0cf0650e54ad79c0f4339dec10780
Wawa27 <32011804+Wawa27@users.noreply.github.com>
2021-03-30 16:14:20 +0200

    test: add login tests (#4)

--- src/main/java/JeeGrp5/mediatech/config/SessionConfiguration.java
+++ src/main/java/JeeGrp5/mediatech/config/SessionConfiguration.java
@@ -1,16 +1,7 @@
 package JeeGrp5.mediatech.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.session.data.mongo.JdkMongoSessionConverter;
 import org.springframework.session.data.mongo.config.annotation.web.http.EnableMongoHttpSession;
 
-import java.time.Duration;
-
-@EnableMongoHttpSession
+@EnableMongoHttpSession(maxInactiveIntervalInSeconds = 3600)
 public class SessionConfiguration {
-
-    @Bean
-    public JdkMongoSessionConverter mongoSessionConverter() {
-        return new JdkMongoSessionConverter(Duration.ofMinutes(15));
-    }
 }
